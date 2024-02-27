b7ea642e1dc0d1c58e0163448ae9539203ab5f8f
= <=>
2024-02-27 07:11:53 -0300

    changes corrections

--- src/app/UI/components/CardPoject.js
+++ src/app/UI/components/CardPoject.js
@@ -10,8 +10,8 @@ export default function CardProject({ project, locale, projects_home_texts, sele
         <>
             <div className={`top-5 lg:top-auto transition-all duration-300 ease-out grid place-content-center absolute ${(index === selected) ? 'w-full z-10' : (index === ant) ? `lg:mr-80` : (index === pos) ? `lg:ml-80` : 'hidden'}`}>
                 <div className="relative text-transparent group transition-all duration-300 ease-in-out">
-                    <div className={`w-full hidden group-hover:text-white lg:grid absolute ${index != selected ? 'hidden' : ''}`} >
-                        <div className={`w-8/12 m-auto py-6 px-5 h-80 group-hover:bg-[rgba(0,0,0,0.5)]`}>
+                    <div className={`w-full h-full hidden group-hover:text-white lg:grid lg:place-content-center absolute ${index != selected ? 'hidden' : ''}`} >
+                        <div className={`w-11/12 m-auto px-5 group-hover:bg-[rgba(0,0,0,0.5)]`}>
                             <h3 className="w-8/12 m-auto text-center z-10 py-6 text-4xl font-bold px-4 rounded-xl group-hover:bg-[rgba(0,0,0,0.8)]">
                                 {locale === 'es' ? project.name_es : project.name_en}
                             </h3>
@@ -23,7 +23,7 @@ export default function CardProject({ project, locale, projects_home_texts, sele
                                     {projects_home_texts.url}
                                 </Link>
                             }
-                            <div className={`hidden group-hover:flex flex-row gap-2 mt-8 justify-evenly px-4`}>
+                            <div className={`hidden group-hover:grid grid-cols-3 gap-2 mt-8 justify-evenly px-4`}>
                                 {
                                     project.tecnologies.map((tecnologie, index) =>
                                         <p key={index} className={`px-4 py-2 rounded-2xl bg-blue-800`}>{tecnologie}</p>
@@ -36,7 +36,7 @@ export default function CardProject({ project, locale, projects_home_texts, sele
                 </div>
             </div>
             <div className={`${index != selected ? 'hidden' : ''} md:hidden`}>
-                <h3 className="w-11/12 text-rose-300 m-auto text-center z-10 py-6 text-3xl font-bold px-4 rounded-xl ">
+                <h3 className="w-11/12 text-rose-300 m-auto text-center z-10 py-6 text-3xl font-bold mt-32 ">
                     {locale === 'es' ? project.name_es : project.name_en}
                 </h3>
                 <Link href={project.github} target="_blank" className="text-rose-200 font-bold w-40 m-auto mt-4 grid place-content-center py-2 rounded-lg mb-3 shadow-[2px_2px_5px_1px_rgba(255,255,255,0.5)] bg-rose-900 bg-opacity-80">
@@ -47,6 +47,13 @@ export default function CardProject({ project, locale, projects_home_texts, sele
                         {projects_home_texts.url}
                     </Link>
                 }
+                <div className={`group-hover:grid grid grid-cols-2 lg:hidden gap-2 mt-8 justify-evenly px-4`}>
+                                {
+                                    project.tecnologies.map((tecnologie, index) =>
+                                        <p key={index} className={`px-4 py-2 rounded-2xl bg-blue-800`}>{tecnologie}</p>
+                                    )
+                                }
+                            </div>
             </div>
         </>
 
