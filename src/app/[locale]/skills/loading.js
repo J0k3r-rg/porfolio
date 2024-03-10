cb7024af59e3915408750dfb447a2c30f06a81d3
J0k3r-rg <pdasriogallegos@gmail.com>
2024-03-10 16:31:53 -0300

    test loading

---
+++ src/app/[locale]/skills/loading.js
@@ -0,0 +1,10 @@
+
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <h1 className="text-4xl text-black">Loading...</h1>
+        </div>
+    );
+}
