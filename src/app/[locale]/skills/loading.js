45b96759b486d3f1a4cec1aea145b448827d1301
J0k3r-rg <pdasriogallegos@gmail.com>
2024-03-10 16:50:36 -0300

    test skeleton

--- src/app/[locale]/skills/loading.js
+++ src/app/[locale]/skills/loading.js
@@ -1,10 +1,18 @@
+'use client'
+import { Skeleton } from "@mui/material";
 
 
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     return (
         <div className="w-full h-full flex justify-center items-center">
-            <h1 className="text-4xl text-black">Loading...</h1>
+            {
+                () => setInterval(() => {
+                    console.log("Loading...");
+                }
+                , 2000)
+            }
+            <Skeleton variant="rectangular" width={210} height={118} />
         </div>
     );
 }
